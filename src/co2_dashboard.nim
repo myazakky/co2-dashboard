bc1a2a65a24f1703393f89c82dedd019b1f9b6db
nimu7 <nimuC@outlook.com>
2021-03-01 22:08:53 +0900

    データを自動で更新するようにした

--- src/co2_dashboard.nim
+++ src/co2_dashboard.nim
@@ -1,9 +1,35 @@
+import json, strformat, dom
 include karax/prelude
+import karax/kajax
+
+include config
+
+let headers = [
+  ("authorization".cstring, (fmt"Bot {token}").cstring),
+  ("Content-Type".cstring, "application/json".cstring)
+]
 
 type
   Room = ref object
     name*: string
+    id*: string
     concentration*: int
+
+proc update(room: Room) =
+  ajaxGet(url = (fmt"https://discordapp.com/api/channels/{room.id}/messages").cstring,
+          headers = headers,
+          cont = proc (status: int, response: cstring) =
+              if status != 200: return
+              
+              let data = ($response).parseJson
+              room.concentration = data[0]["content"].str.parseInt)
+
+var rooms = @[
+  Room(name: node[0].name, concentration: 0, id: node[0].id),
+  Room(name: node[1].name, concentration: 0, id: node[1].id),
+  Room(name: node[2].name, concentration: 0, id: node[2].id),
+  Room(name: node[3].name, concentration: 0, id: node[3].id)
+]
 
 func isGood(room: Room): bool =
   room.concentration < 1000
@@ -20,13 +46,6 @@ func makeHeader(): VNode =
   buildHtml(tdiv(class = "header")):
     p(class = "title"): text "換気状況測定中"
 
-var rooms = @[
-  Room(name: "大ホール", concentration: 600),
-  Room(name: "中ホール", concentration: 600),
-  Room(name: "展示ホール", concentration: 600),
-  Room(name: "大ホール前", concentration: 9999)
-]
-
 proc createDom(): VNode =
   buildHtml(tdiv):
     makeHeader()
@@ -34,4 +53,10 @@ proc createDom(): VNode =
       for room in rooms:
         room.toHtml
 
-setRenderer createDom
+proc update(instance: KaraxInstance) =
+  for room in rooms: room.update
+
+var instance = setRenderer createDom
+
+proc loop() = update(instance)
+discard window.setInterval(loop, 10000)
